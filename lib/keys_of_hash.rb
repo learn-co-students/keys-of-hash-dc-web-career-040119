aa4eb3aadc2ade07367e2b0b885173181088a6d8
Lige <lige.williamson@gmail.com>
2019-02-23 06:46:22 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,12 @@
 require 'pry'
 class Hash
-  def keys_of(arguments)
-    # code goes here
-    binding.pry
+  def keys_of(*arguments)
+    matches = []
+    arguments.each do |item|
+      self.collect do |k, v|
+        matches << k if v == item
+      end
+    end
+    matches.flatten
   end
 end
