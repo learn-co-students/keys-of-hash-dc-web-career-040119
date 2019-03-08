baef9b659a0fd33e4a2d1a6f43fc418d98cf0954
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-08 00:15:15 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+     arguments.each do |animals|
+       self.each do |animal, country|
+         if country == animals
+           array << animal
+          end
+        end
+      end
+      return array
+    end
   end
-end
